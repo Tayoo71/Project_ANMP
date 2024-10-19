89bddca2adf4776a9858208aada72d9948bcae53
Emmanuel <chandra.victorius@gmail.com>
2024-10-19 07:03:29 +0700

    buat who we are

--- app/src/main/java/com/example/project_anmp/view/WhoWeAreFragment.kt
+++ app/src/main/java/com/example/project_anmp/view/WhoWeAreFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.project_anmp.R
 
 class WhoWeAreFragment : Fragment() {
+
+    private var likeCount = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_who_we_are, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_who_we_are, container, false)
 
+        val likeButton: Button = view.findViewById(R.id.button)
+        val likeTextView: TextView = view.findViewById(R.id.textView3)
+
+        likeButton.setOnClickListener {
+            likeCount++
+            likeTextView.text = likeCount.toString()
+        }
+
+        return view
+    }
 }
